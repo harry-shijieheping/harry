578c0b5426f98b998902494af4b7bda127d44771
harry <1562626244@qq.com>
2023-06-09 14:42:05 +0800

    REMDME.md

--- hello.go
+++ hello.go
@@ -1,22 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "C"
 
 func main() {
-	result := 0
-	for i := 0; i <= 10; i++ {
-		result = fiboacci(i)
-		fmt.Printf("fiboacci(%d) is: %d\n", i, result)
-	}
-}
 
-func fiboacci(n int) (res int) {
-	if n <= 1 {
-		res = 1
-	} else {
-		res = fiboacci(n-1) + fiboacci(n-2)
-	}
-	return
+}
+func Random() int {
+	return int(C.random())
+}
+func Seed(i int) {
+	C.srandom(C.uint(i))
 }
